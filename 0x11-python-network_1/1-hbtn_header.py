a56dbe754afc48850017f585d1daf24aa95edf80
root <valentinoaddy@gmail.com>
2024-04-26 16:37:02 +0000

    1 git commit -m 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,21 @@
-
-n script that takes in a URL, sends a request to the URL and displays
-the value of the X-Request-Id variable found in the header of the response.
+#!/usr/bin/python3
+"""
+Displays the value of the X-Request-Id variable
+found in the header of the response
 """
 import urllib.request
-import sys
+from sys import argv
 
+def main(argv):
+    """
+    Method that takes in a URL, sends a request to the URL
+    and displays the value of the X-Request-Id variable
+    found in the header of the response
+    """
+    url = argv
+    with urllib.request.urlopen(url) as response:
+        headers = response.info()
+        print(headers['X-Request-Id'])
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        html = response.info()
-        value = html.get('X-Request-Id')
-        print(value)
-
-
+    main(argv[1])
