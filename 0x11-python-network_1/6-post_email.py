cf3318c11b8c3ed67ad8a37b4afdb5fdefc9bb33
root <valentinoaddy@gmail.com>
2024-04-26 17:14:07 +0000

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,21 @@
-cript that takes in a URL and an email, sends a POST request to the passed
-URL with the email as a parameter, and displays the body of the response.
+
+#!/usr/bin/python3
+"""
+POST request to the passed URL with the email as a parameter
 """
 import requests
-import sys
+from sys import argv
 
+
+def main(argv):
+    """
+    Sends a POST request to the passed URL with the email as a parameter,
+    and displays the body of the response (decoded in utf-8)
+    """
+    values = {'email': argv[2]}
+    url = argv[1]
+    r = requests.post(url, data=values)
+    print(r.text)
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-    payload = {
-        "email": email
-    }
-    response = requests.post(url, data=payload)
-    print(response.text)
-
+    main(argv)
